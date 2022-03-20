6f24891303f3a02bd9272d7dc5dc139d8c03175f
dept60 <karsonziad@gmail.com>
2022-03-20 19:43:33 +0200

    Finished task 4

---
+++ Task 4/Problem7_20210507.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+map<char, int> romanNumerals{
+    {'I', 1},
+    {'V', 5},
+    {'X', 10},
+    {'L', 50},
+    {'C', 100},
+    {'D', 500},
+    {'M', 1000}};
+
+int main()
+{
+    // Take input
+    cout << "Roman numeral: ";
+    string input;
+    cin >> input;
+    // Sum will contain final number
+    int sum = 0;
+    
+    // Loop over the input
+    for (int i = 0; i < input.length(); i++)
+    {
+        input[i] = toupper(input[i]);
+        
+        // if the roman numeral is smaller than the one after it,
+        // then it will be considered negative (like IV is -1 + 5)
+        // otherwise it's positive
+        if (i < input.length() - 1 && romanNumerals[input[i]] < romanNumerals[input[i + 1]])
+        {
+            sum -= romanNumerals[input[i]];
+        }
+        else
+        {
+            sum += romanNumerals[input[i]];
+        }
+    }
+    // Output
+    cout << input << " -> " << sum << endl;
+    system("pause");
+};
